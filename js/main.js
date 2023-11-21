16de2e9bf903f531fa016733d8b35ca6abb6e672
AlbertoMessineo <albevorox@gmail.com>
2023-11-21 17:15:59 +0100

    feat: object array

--- js/main.js
+++ js/main.js
@@ -1,72 +1,94 @@
 'use strict';
 
-/* <div class="slide ">
-<img src="img/01.jpg" alt="img-1">
-</div>
-<div class="slide">
-<img src="img/02.jpg" alt="">
-</div>
-<div class="slide active">
-<img src="img/03.jpg" alt="">
-</div>
-<div class="slide">
-<img src="img/04.jpg" alt="">
-</div>
-<div class="slide">
-<img src="img/05.jpg" alt="">
-</div> */
 
 
 // MARK UP HTML
- const images = ['01.jpg' , '02.jpg', '03.jpg' ,'04.jpg' , '05.jpg' ];
- const slider = document.querySelector('.slider');
- let currentSlide = 0;
- const prev = document.querySelector('.prev');
- const next = document.querySelector('.next');
+// const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+// const slider = document.querySelector('.slider');
+// let currentSlide = 0;
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
 
- for( let i = 0; i < images.length; i++){
+// for (let i = 0; i < images.length; i++) {
 
-    //slide
-    const slide = document.createElement('div');
-    slide.classList.add('slide');
+//     //slide
+//     const slide = document.createElement('div');
+//     slide.classList.add('slide');
 
-    if(i === currentSlide){
-        slide.classList.add('active');
+//     if (i === currentSlide) {
+//         slide.classList.add('active');
+//     }
+
+//     //img
+//     const img = document.createElement('img');
+//     img.src = `img/${images[i]}`;
+//     img.alt = `Carousel/${images[i]}`;
+
+//     slide.append(img);
+//     slider.append(slide);
+
+// }
+
+// OBJECT ARRAY CREATION
+let imagesArray = [
+    {
+        url: '<img src="img/01.jpg" alt="img-1">',
+        title: 'Immagine Carosello 1',
+        caption: 'Immagine stupenda',
+    },
+    {
+        url: '<img src="img/02.jpg" alt="img-2">',
+        title: 'Immagine Carosello 2',
+        caption: 'Immagine stupenda',
+    },,
+    {
+        url: '<img src="img/03.jpg" alt="img-3">',
+        title: 'Immagine Carosello 3',
+        caption: 'Immagine stupenda',
+    },
+    {
+        url: '<img src="img/04.jpg" alt="img-4">',
+        title: 'Immagine Carosello 4',
+        caption: 'Immagine stupenda',
+    },
+    {
+        url: '<img src="img/05.jpg" alt="img-5">',
+        title: 'Immagine Carosello 5',
+        caption: 'Immagine stupenda',
+    },
+    {
+        url: '<img src="img/06.jpg" alt="img-6">',
+        title: 'Immagine Carosello 6',
+        caption: 'Immagine stupenda',
     }
+];
+console.log(imagesArray);
 
-    //img
-    const img = document.createElement('img');
-    img.src = `img/${images[i]}`;
-    img.alt = `Carousel/${images[i]}`;
 
-    slide.append(img);
-    slider.append(slide);
-
- }
 
 const domSlides = document.querySelectorAll('.slide');
 
 //  SLIDER EVENTS
- prev.addEventListener('click', function(){
+prev.addEventListener('click', function () {
     console.log("Ho cliccato su prev");
 
-if(currentSlide > 0 ){
+    if (currentSlide > 0) {
 
-    domSlides[currentSlide].classList.remove('active');
-    currentSlide--;
-    domSlides[currentSlide].classList.add('active');
-}
- });
+        domSlides[currentSlide].classList.remove('active');
+        currentSlide--;
+        domSlides[currentSlide].classList.add('active');
+    }
+});
 
- next.addEventListener('click', function(){
+next.addEventListener('click', function () {
     console.log("Ho cliccato su next");
 
-    if(currentSlide < domSlides.length - 1){
+    if (currentSlide < domSlides.length - 1) {
 
         domSlides[currentSlide].classList.remove('active');
         currentSlide++;
         domSlides[currentSlide].classList.add('active');
-        
+
     }
- });
+});
 
